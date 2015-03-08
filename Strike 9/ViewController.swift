cef85609982fdf8f22e152c9a4d622646d734caf
Jose Zamudio <zamudio@outlook.com>
2015-03-08 13:25:21 -0700

    Initial Commit

---
+++ Strike 9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Strike 9
+//
+//  Created by Jose Zamudio on 3/8/15.
+//  Copyright (c) 2015 zamudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Strike 9Tests/Strike_9Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Strike_9Tests.swift
+//  Strike 9Tests
+//
+//  Created by Jose Zamudio on 3/8/15.
+//  Copyright (c) 2015 zamudio. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Strike_9Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
