4ea9dbe39bcd547151b235042c24bfa3ce19beb3
Jose Zamudio <zamudio@outlook.com>
2015-03-08 14:02:10 -0700

    Add README.md, initial testing

---
+++ README.md
@@ -0,0 +1,2 @@
+# Strike 9
+Game developed as part of the [Coding House](http://codinghouse.co) code-for-good initiative
--- Strike 9/ViewController.swift
+++ Strike 9/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	var topView:UIView = UIView()
+	var middleView:UIView = UIView()
+	var bottomView:UIView = UIView()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		self.view.addSubview(drawTopView())
+		self.view.addSubview(drawMiddleView())
+		self.view.addSubview(drawBottomView())
+	}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	func drawTopView() -> UIView {
+		topView = UIView(frame: CGRectMake(0, 0, self.view.frame.width, self.view.frame.height * 0.2))
+		topView.backgroundColor = UIColor.redColor()
+		return topView
+	}
+	
+	func drawMiddleView() -> UIView {
+		middleView = UIView(frame: CGRectMake(0, topView.bounds.height, self.view.bounds.width, self.view.frame.height * 0.6))
+		middleView.backgroundColor = UIColor.blackColor()
+		return middleView
+	}
+	
+	func drawBottomView() -> UIView {
+		bottomView = UIView(frame: CGRectMake(0, topView.bounds.height + self.middleView.bounds.height, self.view.frame.width, self.view.frame.height * 0.2))
+		bottomView.backgroundColor = UIColor.greenColor()
+		return bottomView
+	}
+	
+	func createGrid() -> UIView {
+		
+	}
+	
 }
 
